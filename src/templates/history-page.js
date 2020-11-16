1ba73d067b0991efd777b7f4a3a746d26d362dc5
Nemenaja <nemanjapuw@gmail.com>
2020-11-16 08:21:21 +0100

    add History collection, 2 pages for history1, history2

---
+++ src/templates/history-page.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const HistoryPageTemplate = ({ title, heading, content, contentComponent }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h1 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h1>
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {heading}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+HistoryPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  heading: PropTypes.string,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const HistoryPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <HistoryPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        heading={post.frontmatter.heading}
+        content={post.frontmatter.main.description}
+      />
+    </Layout>
+  )
+}
+
+HistoryPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default HistoryPage
+
+export const historyPageQuery = graphql`
+  query HistoryPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        heading
+        main {
+          description
+        }
+      }
+    }
+  }
+`
